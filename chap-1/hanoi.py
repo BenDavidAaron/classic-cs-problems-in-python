d14862ea4e053a21b1413e253559277c72b633f4
Ben Aaron <benjamin_aaron@bio-rad.com>
2019-05-23 16:42:39 -0700

    instantiate the three towers

--- chap-1/hanoi.py
+++ chap-1/hanoi.py
@@ -14,3 +14,10 @@ class Stack(Generic[T]):
 
     def __repr__(self) -> str:
         return repr(self._container)
+
+num_discs: int = 3
+tower_a: Stack[int] = Stack()
+tower_b: Stack[int] = Stack()
+tower_c: Stack[int] = Stack()
+for i in range(1, num_discs + 1):
+    tower_a.push(i)
